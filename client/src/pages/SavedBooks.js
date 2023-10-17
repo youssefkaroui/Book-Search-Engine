14227971ece627a6e7efa92714e33e91b1d9158f
Youssef <youssefkaroui6@gmail.com>
2023-10-17 19:53:50 -0400

    started working on SavedBooks.js to implement apollo server and make the changes needed

--- client/src/pages/SavedBooks.js
+++ client/src/pages/SavedBooks.js
@@ -7,9 +7,13 @@ import {
   Col
 } from 'react-bootstrap';
 
-import { getMe, deleteBook } from '../utils/API';
-import Auth from '../utils/auth';
-import { removeBookId } from '../utils/localStorage';
+import { useQuery, useMutation } from "@apollo/react-hooks";
+import { removeBookId } from "../utils/localStorage";
+import Auth from "../utils/auth";
+import { GET_ME } from "../utils/queries";
+import { REMOVE_BOOK } from "../utils/mutations";
+
+const userData = data?.me || [];
 
 const SavedBooks = () => {
   const [userData, setUserData] = useState({});
@@ -51,14 +55,10 @@ const SavedBooks = () => {
     }
 
     try {
-      const response = await deleteBook(bookId, token);
+      await removeBook({
+        variables: { bookId },
+      });
 
-      if (!response.ok) {
-        throw new Error('something went wrong!');
-      }
-
-      const updatedUser = await response.json();
-      setUserData(updatedUser);
       // upon success, remove book's id from localStorage
       removeBookId(bookId);
     } catch (err) {
@@ -67,9 +67,10 @@ const SavedBooks = () => {
   };
 
   // if data isn't here yet, say so
-  if (!userDataLength) {
+  if (loading) {
     return <h2>LOADING...</h2>;
   }
+
 
   return (
     <>
